c09d65496ab5226611e05723c1ecca1e75ecb45a
34TW <tamex.worku@gmail.com>
2022-10-16 05:51:51 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- the strat of the program
+ * Description: print puts sentence
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
