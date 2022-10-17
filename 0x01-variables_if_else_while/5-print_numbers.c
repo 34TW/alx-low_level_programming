48fe8c05675107c277ca5ba4ba416d781104a6a5
34TW <tamex.worku@gmail.com>
2022-10-17 05:07:46 -0700

    number zero to ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- the start of the program
+ * Description: print zero to ten
+ * Return: always 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		putchar(x);
+	}
+		putchar('\n');
+	return (0);
+}
+
+
