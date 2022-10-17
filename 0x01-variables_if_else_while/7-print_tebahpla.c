c53fec80eb89de2681fd5ccc548b660a9070664a
34TW <tamex.worku@gmail.com>
2022-10-17 05:40:00 -0700

    inverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the start of the program
+ * Description: print inverse alphabet
+ * Return 0
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a')
+		{
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
+
