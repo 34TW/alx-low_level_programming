5787ddf6647b0c70b16998b0b106597d5a225107
34TW <tamex.worku@gmail.com>
2022-10-21 16:01:40 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line- function print the line
+ * @n: teh integer parameter
+ * Return: void
+ */
+void print_line(int n)
+{
+	int m;
+
+	if (n <= 0)
+	_putchar('\n');
+	for (m = 0 ; m <= n ; m++)
+	{
+		_putchar(95);
+	}
+		_putchar('\n');
+}
+
