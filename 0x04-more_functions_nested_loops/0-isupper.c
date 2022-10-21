aa9b500aa92f72e44b98c463768ca9f8d3eafbdd
34TW <tamex.worku@gmail.com>
2022-10-21 03:38:22 -0700

    check function for uppercase parameter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isupper- check function for uppercase
+* @c: the character
+* Return: 1 for uppercase otherwise 0
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+
+}
