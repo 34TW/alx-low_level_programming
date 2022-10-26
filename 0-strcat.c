44f79344f4bc823190e89c28fa8f089ad3a4ae9a
34TW <tamex.worku@gmail.com>
2022-10-26 11:56:55 -0700

    concatenates two strings.

---
+++ 0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat- the start of the function
+ * @dest: first parameter
+ * @src: the second paramter
+ * Return: pointer dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
