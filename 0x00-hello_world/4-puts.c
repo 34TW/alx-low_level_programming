6c9f4b99ea1294afdf65c090badf109a6fac694b
34TW <tamex.worku@gmail.com>
2023-03-16 11:20:13 -0700

    update header file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-include < stdio.h >
+#include <stdio.h>
 /**
  * main- A program that print a line with puts function
- * void: no parameter to the function
-  * return: 0 (success)
+  * return: (0) success
  */
 void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return ();
+	return (0);
 }
