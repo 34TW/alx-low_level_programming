ce494c30f3e1301fc426845906897f94430977db
34TW <tamex.worku@gmail.com>
2023-03-16 11:06:19 -0700

    puts print exactly line word

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+include < stdio.h >
+/**
+ * main- A program that print a line with puts function
+ * void: no parameter to the function
+  * return: 0 (success)
+ */
+void main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return ();
+}
