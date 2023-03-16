9b08cab180e5ca12308edfd91fb364f55a7007bc
34TW <tamex.worku@gmail.com>
2023-03-16 11:29:30 -0700

    make file executable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main- A program that print a line with puts function
-  * return: (0) success
+  * return:always 0  success
  */
-void main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
