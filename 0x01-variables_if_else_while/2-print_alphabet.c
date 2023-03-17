f822be6e007a98b4631f5a349456b05aa06795c1
34TW <tamex.worku@gmail.com>
2023-03-17 11:58:50 -0700

    print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- fumction prints lower case alphabet
+ * Return: always 0 success
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
