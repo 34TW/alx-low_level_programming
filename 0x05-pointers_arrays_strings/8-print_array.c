b867386cea75c128c1935ed9c07dacf4d96614c0
34TW <tamex.worku@gmail.com>
2023-03-28 19:53:49 -0700

    print n array with comma

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array-function print n array
+ *@a:parameter first int
+ *@n:parameter second int
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d,", a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d,", a[n - 1]);
+	}
+	printf("\n");
+}
