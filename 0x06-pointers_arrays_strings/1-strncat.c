4651d98e9af2e973aa070d2952dfa35e0bb7d23d
34TW <tamex.worku@gmail.com>
2023-03-30 10:56:03 -0700

    concatenat two string contain still int n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strncat- function concatenate two string with int n
+ *@dest:parameter first char
+ *@src:parameter second char
+ *@n:parameter third int
+ *Return:always dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
