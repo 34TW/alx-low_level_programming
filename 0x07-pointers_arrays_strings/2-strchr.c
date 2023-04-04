4e7c2afccb74c3c9f63b6ffd652c95ab46d46f2d
34TW <tamex.worku@gmail.com>
2023-04-04 11:05:58 -0700

    pointer to the first occurrence of the c in the string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strchr-function check first char in array pointer
+ *@s:parameter first char pointer
+ *@c:parameter second char
+ *Return:char else NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
