6deff78027b20714798c9a56c7e5a9665f95f7e5
TW34 <tamex.worku@gmail.com>
2023-04-17 14:04:36 -0700

    function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+include "dog.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d. == NULL)
+		printf("nil");
+	if (d.name == NULL)
+		printf("Name: (nil)");
+				else (d == NULL)
+				printf("NULL");
+
