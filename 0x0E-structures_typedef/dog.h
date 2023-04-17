9fb529bdd5799d4258af56290864405949dcd920
TW34 <tamex.worku@gmail.com>
2023-04-17 12:11:18 -0700

    reademe.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include "main.h"
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+int main()
+{
+	struct dog my_dog;
+	my_dog.name = 
+		my_dog.age = 
+		my_dog.owner =
+	       printf("My name is %s, and i am age %f", my_dog.name, my_dog.age	
