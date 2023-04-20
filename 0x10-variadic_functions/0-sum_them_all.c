54b81605e8c8d37b3b0923accef12639fb399378
TW34 <tamex.worku@gmail.com>
2023-04-20 09:03:06 -0700

    betty error correct

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,18 +4,19 @@
 /**
 *sum_them_all- function intry point
 *@n: argument to constant int
-*Return : always int success
+*Return: always int sum
 */
+
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
 int sum = 0;
 va_list ap;
-va_start (ap, n);
+va_start(ap, n);
 
 for (i = 0; i < n; i++)
-sum = sum + va_arg (ap, int);
+sum = sum + va_arg(ap, int);
 
-va_end (ap);
+va_end(ap);
 return (sum);
 }
